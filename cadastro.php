2ca573ec672e43ccd8d6190bffd0163b6a9e0d66
henriquecorreia1 <henri741@gmail.com>
2017-04-18 10:21:23 -0300

    #POST
    coloquei as option dentro do metodo post para enviar para o banco de dados

--- cadastro.php
+++ cadastro.php
@@ -23,75 +23,75 @@
 
 		Curso: <br>
 
-			<select>
-				<option>Administração</option>
-				<option>Agronomia</option>
-				<option>Arquitetura e Urbanismo</option>
-				<option>Arquivologia</option>
-				<option>Artes Cênicas</option>
-				<option>Artes Plásticas</option>
-				<option>Biblioteconomia</option>
-				<option>Ciência da Computação </option>
-				<option>Ciência Política</option>
-				<option>Ciências Ambientais</option>
-				<option>Ciências Biológicas</option>
-				<option>Ciências Contábeis</option>
-				<option>Ciências Econômicas</option>
-				<option>Ciências Farmacêuticas</option>
-				<option>Ciências Naturais</option>
-				<option>Ciências Sociais</option>
-				<option>Comunicação Organizacional</option>
-				<option>Comunicação Social </option>
-				<option>Design</option>
-				<option>Direito </option>
-				<option>Educação Física </option>
-				<option>Educação do Campo</option>
-				<option>Enfermagem e Obstetrícia</option>
-				<option>Engenharia Aeroespacial</option>
-				<option>Engenharia Ambiental </option>
-				<option>Engenharia Automotiva</option>
-				<option>Engenharia Civil</option>
-				<option>Engenharia de Computação</option>
-				<option>Engenharia de Energia</option>
-				<option>Engenharia de Redes de Comunicação</option>
-				<option>Engenharia de Produção</option>
-				<option>Engenharia de Software</option>
-				<option>Engenharia Elétrica</option>
-				<option> Engenharia Eletrônica</option>
-				<option>Engenharia Florestal</option>
-				<option>Engenharia Mecânica</option>
-				<option>Engenharia Mecatrônica</option>
-				<option>Estatística</option>
-				<option>Farmácia</option>
-				<option>Filosofia</option>
-				<option>Física</option>
-				<option>Fisioterapia</option>
-				<option>Fonoaudiologia</option>
-				<option>Geofísica</option>
-				<option>Geografia</option>
-				<option>Geologia</option>
-				<option>Gestão Ambiental</option>
-				<option>Gestão de Políticas Públicas</option>
-				<option>Gestão de Agronegócio</option>
-				<option>Gestão em Saúde Coletiva</option>
-				<option>História</option>
-				<option>Letras</option>
-				<option>Matemática</option>
-				<option>Medicina</option>
-				<option>Medicina Veterinária</option>
-				<option>Museologia</option>
-				<option>Música</option>
-				<option>Nutrição</option>
-				<option>Odontologia</option>
-				<option>Pedagogia</option>
-				<option>Psicologia</option>
-				<option>Química</option>
-				<option>Química Tecnológica</option>
-				<option>Relações Internacionais</option>
-				<option>Serviço Social</option>
-				<option>Terapia Ocupacional</option>
-				<option>Turismo</option>
-				<option>Visuais</option>
+			<select name = "course">
+				<option value = "Administração">Administração</option>
+				<option value = "Agronomia">Agronomia</option>
+				<option value = "Arquitetura e Urbanismo">Arquitetura e Urbanismo</option>
+				<option value = "Arquivologia">Arquivologia</option>
+				<option value = "Artes Cênicas">Artes Cênicas</option>
+				<option value = "Artes Plásticas">Artes Plásticas</option>
+				<option value = "Biblioteconomia">Biblioteconomia</option>
+				<option value = "Ciência da Computação">Ciência da Computação </option>
+				<option value = "Ciência Política">Ciência Política</option>
+				<option value = "Ciências Ambientais">Ciências Ambientais</option>
+				<option value = "Ciências Biológicas">Ciências Biológicas</option>
+				<option value = "Ciências Contábeis">Ciências Contábeis</option>
+				<option value = "Ciências Econômicas">Ciências Econômicas</option>
+				<option value = "Ciências Farmacêuticas">Ciências Farmacêuticas</option>
+				<option value = "Ciências Naturais">Ciências Naturais</option>
+				<option value = "iências Sociais">Ciências Sociais</option>
+				<option value = "Comunicação Organizacional">Comunicação Organizacional</option>
+				<option value = "Comunicação Social">Comunicação Social </option>
+				<option value = "Design">Design</option>
+				<option value = "Direito ">Direito </option>
+				<option value = "Educação Física">Educação Física </option>
+				<option value = "Educação do Campo">Educação do Campo</option>
+				<option value = "Enfermagem e Obstetrícia">Enfermagem e Obstetrícia</option>
+				<option value = "Engenharia Aeroespacial">Engenharia Aeroespacial</option>
+				<option value = "Engenharia Ambiental">Engenharia Ambiental </option>
+				<option value = "Engenharia Automotiva">Engenharia Automotiva</option>
+				<option value = "Engenharia Civil">Engenharia Civil</option>
+				<option value = "Engenharia de Computação">Engenharia de Computação</option>
+				<option value = "Engenharia de Energia">Engenharia de Energia</option>
+				<option value = "Engenharia de Redes de Comunicação">Engenharia de Redes de Comunicação</option>
+				<option value = "Engenharia de Produção">Engenharia de Produção</option>
+				<option value = "Engenharia de Software">Engenharia de Software</option>
+				<option value = "Engenharia Elétrica">Engenharia Elétrica</option>
+				<option value = "Engenharia Eletrônica"> Engenharia Eletrônica</option>
+				<option value = "Engenharia Florestal">Engenharia Florestal</option>
+				<option value = "Engenharia Mecânica">Engenharia Mecânica</option>
+				<option value = "Engenharia Mecatrônica">Engenharia Mecatrônica</option>
+				<option value = "Estatística">Estatística</option>
+				<option value = "Farmácia">Farmácia</option>
+				<option value = "Filosofia">Filosofia</option>
+				<option value = "Física">Física</option>
+				<option value = "Fisioterapia">Fisioterapia</option>
+				<option value = "Fonoaudiologia">Fonoaudiologia</option>
+				<option value = "Geofísica">Geofísica</option>
+				<option value = "Geografia">Geografia</option>
+				<option value = "Geologia">Geologia</option>
+				<option value = "Gestão Ambiental">Gestão Ambiental</option>
+				<option value = "Gestão de Políticas Públicas">Gestão de Políticas Públicas</option>
+				<option value = "Gestão de Agronegócio">Gestão de Agronegócio</option>
+				<option value = "Gestão em Saúde Coletiva">Gestão em Saúde Coletiva</option>
+				<option value = "História">História</option>
+				<option value = "Letras">Letras</option>
+				<option value = "Matemática">Matemática</option>
+				<option value = "Medicina">Medicina</option>
+				<option value = "Medicina Veterinária">Medicina Veterinária</option>
+				<option value = "Museologia">Museologia</option>
+				<option value = "Música">Música</option>
+				<option value = "Nutrição">Nutrição</option>
+				<option value = "Odontologia">Odontologia</option>
+				<option value = "Pedagogia">Pedagogia</option>
+				<option value = "Psicologia">Psicologia</option>
+				<option value = "Química">Química</option>
+				<option value = "Química Tecnológica">Química Tecnológica</option>
+				<option value = "Relações Internacionais">Relações Internacionais</option>
+				<option value = "Serviço Social">Serviço Social</option>
+				<option value = "Terapia Ocupacional">Terapia Ocupacional</option>
+				<option value = "Turismo">Turismo</option>
+				<option value = "Visuais">Visuais</option>
 				
 
 			</select>
