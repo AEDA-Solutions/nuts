c3b52aae3d02e1c6ff30d21c604127c3444bf5b5
SamuelSS09 <samuelss0906@gmail.com>
2017-04-26 20:13:21 -0300

    Renomeado alguns aquivos, deletados outros e refeita a integração entre os codigos

--- index.php
+++ index.php
@@ -1,11 +1,47 @@
+<!doctype html>
 <html>
-<head>
-<title> NUTS </title>
-</head>
-<meta charset="utf-8" />
-<frameset cols="15%,*">  
-<frame src="menu.php">
-<frame src="rede.html" >
-<frame src="loginnuts.html" name="LOGIN">
-</frameset>
+
+	<head>
+		<title>Nuts</title>
+		<meta charset="utf-8" />
+	</head>
+	<body>
+
+		<table border="0" width="900" align="center">
+			<tr>
+				<td> <img src="imagens/logo.png" /> </td>
+				<td align="right">
+					<a href="index.html">Home</a> |
+					<a href="quemsomos.html">Quem somos</a> |
+					<a href="rede.html"> Análise de Rede </a> |
+					<a href="login.html"> Login </a> |
+					<a href="cadastro.html"> Cadastro </a> |
+					<a href="contato.html">Contato</a> 
+				</td>
+			</tr>
+
+			<tr>
+				<td colspan="2">
+					<img src="imagens/quem_somos.png" />
+				</td>
+			</tr>
+
+			<tr>
+				<td colspan="2">
+					<p>
+					Nós, da nuts, temos como objetivo principal de mostrar, a vocês usuários como estão suas redes locais. 
+          QUEREMOS MAIS NESSA BIO   QUEREMOS MAIS NESSA BIO  QUEREMOS MAIS NESSA BIO V QUEREMOS MAIS NESSA BIO  
+          QUEREMOS MAIS NESSA BIO  QUEREMOS MAIS NESSA BIO  QUEREMOS MAIS NESSA BIO  QUEREMOS MAIS NESSA BIO  
+          QUEREMOS MAIS NESSA BIO  QUEREMOS MAIS NESSA BIO  QUEREMOS MAIS NESSA BIO  QUEREMOS MAIS NESSA BIO  
+          QUEREMOS MAIS NESSA BIO </p>
+
+
+				</td>
+			</tr>
+
+
+
+		</table>
+
+	</body>
 </html>
