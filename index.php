aa6031ffda4320f622eff1500a0d8fb998d46e76
MirellaSales <mi.rella_sales@hotmail.com>
2017-05-02 17:47:10 -0300

    Update index.php

--- index.php
+++ index.php
@@ -11,12 +11,12 @@
 			<tr>
 				<td> <img src="imagens/logo.png" /> </td>
 				<td align="right">
-					<a href="index.php">Home</a> |
+			                <a href="index.php">Home</a> |
 					<a href="quemsomos.php">Quem somos</a> |
-					<a href="rede.html"> Análise de Rede </a> |
+					<a href="rede.php"> Análise de Rede </a> |
 					<a href="login.php"> Login </a> |
-					<a href="sign_up.php"> Cadastro </a> |
-					<a href="contacto.html">Contato</a> 
+					<a href="cadastro.php"> Cadastro </a> |
+					<a href="contato.php">Contato</a> 
 				</td>
 			</tr>
 
