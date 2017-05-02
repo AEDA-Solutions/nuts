20318841813eb6e5b66721d7168bbd1ef3a1ae83
samuelss0906@gmail.com <samuelss0906@gmail.com>
2017-05-02 11:15:25 -0300

    Adicionadas funções de perda de pacotes e ping à classe NetController

--- NetController.php
+++ NetController.php
@@ -18,8 +18,9 @@
 
 		//retorna um float com o valor do ping em ms se o servidor responder
 		//retorna false caso o servidor nao responda
-		function ping($host, $port, $timeout) { 
-		  	
+		function checkPing($host, $port, $timeout) { 
+		  	//retorna o ping caso de certo, ou false caso de errado
+
 		  	$tB = microtime(true); 
  		 	$fP = fSockOpen($host, $port, $errno, $errstr, $timeout); 
 		 	if (!$fP) { return false; } 
@@ -31,5 +32,23 @@
 			header('Location: get_location.php');
 		}
 
+
+		function checkPacketLoss($address, $count) {
+    		$command = 'ping -c %d %s';
+    		$output = shell_exec(sprintf($command, $count, $address));
+
+    		if (preg_match('/([0-9]*\.?[0-9]+)%(?:\s+packet)?\s+loss/', $output, $match) === 1) {
+      		  $packetLoss = (float)$match[1]; 
+  			
+  			} 
+
+    		else {
+    			return false;
+        		//throw new \Exception('Packet loss not found.');
+    		}
+
+    
+    		return $packetLoss;
+		}
 	}
 ?>
