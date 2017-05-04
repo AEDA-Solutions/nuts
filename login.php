266f17769cd2617ed96261767e8a0dcd7c07aa22
MirellaSales <mi.rella_sales@hotmail.com>
2017-05-04 15:20:02 -0300

    Update login.php

--- login.php
+++ login.php
@@ -1,12 +1,40 @@
+<!doctype html>
 <html>
+
+
+<stile> 
+<body bgcolor= #FF7F50> 
+</stile>
+
+
+
 <head> 
 <title> Login </title>
 <h1> Login </h1>
 <br>
 </head>
+
+
 <meta charset="utf-8" />
-<body align="justify">
+	<body align="center">
+		<table border="0" width="900" align="center">
+			<tr>
+				<td> <img src="images/nuts.png" /> </td>
+				<td align="right">
+					<a href="index.php">Home</a> |
+					<a href="quemsomos.php">Quem somos</a> |
+					<a href="rede.php"> Análise de Rede </a> |
+					<a href="login.php"> Login </a> |
+					<a href="sign_up.php"> Cadastro </a> |
+					<a href="contato.php">Contato</a> 
+
+				</td>
+			</tr>
+
 <font size="2" face="arial"> 
+
+
+
 <form method="post" action="validate_user.php" >
 Para mais informações sobre a rede, efetue o login.<br>
 	Matrícula: <input type="text" name="id"><br>
@@ -16,7 +44,11 @@ Para mais informações sobre a rede, efetue o login.<br>
 	Você não tem nosso cadastro? clique
 	<a href="cadastro.php" target="principal" > aqui</a> 
 	e cadastre-se!
+
 </form>
 </font>
-</body>
+
+		</table>
+
+	</body>
 </html>
