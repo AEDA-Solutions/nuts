e4940160e5b2f7657b84eed2f99ba256cefc856e
andreanndrade10 <andreanndrade10@gmail.com>
2017-05-09 11:35:17 -0300

    Banco de Dados atualizado.

---
+++ NUTS.sql
@@ -0,0 +1,23 @@
+CREATE DATABASE Nuts;
+
+use `Nuts`;
+
+create table netdata 
+(
+	id int (11) NOT NULL PRIMARY KEY AUTO_INCREMENT,
+    latitude float DEFAULT NULL,
+	longitude float DEFAULT NULL,
+    ping float DEFAULT NULL,
+    packetloss float DEFAULT NULL
+
+);
+
+create table users 
+(
+	id varchar (9) NOT NULL PRIMARY KEY,
+    `name` varchar (100) NOT NULL,
+    email varchar (100) NOT NULL,
+    `password` varchar (32) NOT NULL,
+    course varchar (100) NOT NULL
+
+);
