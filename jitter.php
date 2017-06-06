d0f1ba8a5ed1ffef6b9cd58c7a36f641d4cad248
João <queirozbragajv@gmail.com>
2017-06-06 18:42:28 -0300

    função que calcula o jitter

---
+++ jitter.php
@@ -0,0 +1,24 @@
+<?php 
+
+function ping($host, $port, $timeout) { 
+
+	error_reporting(0);
+	ini_set(“display_errors”, 0 );
+
+	$tB = microtime(true); 
+	$fP = fSockOpen($host, $port, $errno, $errstr, $timeout); 
+	if (!$fP) { return "down"; } 
+	$tA = microtime(true); 
+	$ping1 = round((($tA - $tB) * 1000), 0)." ms"; 
+	return $ping1;
+}
+	$j1 = ping("google.com.br", 80, 10);
+	sleep(2);
+	$j2 = ping("google.com.br", 80, 10);
+	sleep(2);
+	$j3 = ping("google.com.br", 80, 10);
+
+	$soma = ($j1+$j2+$j3)/3;
+
+	echo "Seu Jitter é: $soma";
+?>
