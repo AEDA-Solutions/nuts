9a5658b178241957544ef8d0cfbf8eb6e09a867b
João <queirozbragajv@gmail.com>
2017-06-08 10:59:00 -0300

    Função do jitter implementada

--- NetController.php
+++ NetController.php
@@ -45,6 +45,31 @@
 			return round((($tA - $tB) * 1000), 0); 
 		}
 
+		function check_jitter($host, $port, $timeout) { 
+		 
+		error_reporting(0);
+		ini_set(“display_errors”, 0 );
+
+		$tB = microtime(true); 
+		$fP = fSockOpen($host, $port, $errno, $errstr, $timeout); 
+		if (!$fP) { return "down"; } 
+		$tA = microtime(true); 
+		$ping1 = round((($tA - $tB) * 1000), 0)." ms"; 
+		//return $ping1;
+}
+		$j1 = jitter("google.com.br", 80, 10);
+		sleep(2);
+		$j2 = jitter("google.com.br", 80, 10);
+		sleep(2);
+		$j3 = jitter("google.com.br", 80, 10);
+
+		$soma = ($j1+$j2+$j3)/3;
+
+		//echo "Seu Jitter é: $soma";
+		return $soma;
+		}
+
+
 		function set_location($latitude,$longitude){
 			$this->latitude=$latitude;
 			$this->longitude=$longitude;
