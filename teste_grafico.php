07704fb2d860d0df3cc9cfd002cbe668c8310896
João <queirozbragajv@gmail.com>
2017-06-28 15:28:34 -0300

    função que plota gráficos funcionando

--- teste_grafico.php
+++ teste_grafico.php
@@ -1,15 +1,13 @@
 <?php 
-require_once('phplot-6.2.0/phplot.php');
-require_once('phplot-6.2.0/rgb.inc.php');
+require_once 'phplot/phplot.php';
+//require_once 'phplot/rgb.inc.php';
 
 $plot = new PHPlot;
 
-SetFileFormat("");
-
 #Indicamos o títul do gráfico e o título dos dados no eixo X e Y do mesmo
-$grafico->SetTitle("Gráfico de exemplo");
-$grafico->SetXTitle("Eixo X");
-$grafico->SetYTitle("Eixo Y");
+$plot->SetTitle("Gráfico de exemplo");
+$plot->SetXTitle("Eixo X");
+$plot->SetYTitle("Eixo Y");
 
 
 #Definimos os dados do gráfico
@@ -22,11 +20,11 @@ $dados = array(
 		array('Junho', 5),
 );
 
-$grafico->SetDataValues($dados);
+$plot->SetDataValues($dados);
  
 #Neste caso, usariamos o gráfico em barras
-$grafico->SetPlotType("bars");
+$plot->SetPlotType("bars");
 
 #Exibimos o gráfico
-$grafico->DrawGraph();
+$plot->DrawGraph();
 ?>
