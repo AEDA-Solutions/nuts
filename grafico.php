80f4e8de0e8b306477174507d3311dbdefa19ac7
isabelalobatob <isabela_lobato@hotmail.com>
2017-06-29 14:45:28 -0300

    Mudanças Modal

--- grafico.php
+++ grafico.php
@@ -89,7 +89,7 @@ $user_data = $NC->get_last_data();
 
 
 
-        <div class="modal-dialog modal-lg">
+        <div class="modal-dialog modal-sm">
           <div class="modal-content">
             <div class="modal-header">
               <button type="button" class="close" data-dismiss="modal">
