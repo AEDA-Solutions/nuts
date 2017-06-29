e679ac92c2013fac5a531741246328359028f02b
isabelalobatob <isabela_lobato@hotmail.com>
2017-06-29 00:07:20 -0300

    Atualização Design Tabela.php

--- tabela.php
+++ tabela.php
@@ -114,16 +114,10 @@ function validate_data($d){
             </div>
 
              <div class="modal-body">
-              
-            
 
-            
-           
             </div>
 
              <div class="modal-footer">
-
-              
 
               <button type="submit" class="btn btn-login btn-white">Enviar avaliação</a>
 
@@ -134,205 +128,86 @@ function validate_data($d){
       </form>
 <!-- /Modal de avaliação -->
 
-      <section id="capa-indexusuario">
-        <br>
-        <br>
-        <br>
-        <br>
-        <br>
-        
-      </section>
+<header>
+  <div class="row">
+    
+  </div>
+</header>
+  <!-- TABELAS -->
+  <section id=tabelas>
+   <div class="container">
 
-      <section id="cadastro">
-    <div class="container">
-      <div id="cadastro" class="cadastro">
-      <h1>Análise da Rede</h1>
-      <h4>Aqui estão os dados analisados da sua rede!</h4> 
-      <hr>
-      <div class="row">
-       
-        
-        <table class="table table-inverse">
+   <div class="tabelas-titulos">
+     <div class="row">
+       <h1>Análise Avançada da sua Rede</h1>
+       <h4>DADOS COMPLETOS DA SUA REDE ATUAL</h4>
+     </div>
+   </div>
+
+    <div class="row">
+
+      <!-- TABELA LOCAL -->
+        <div class="col-md-6 col-md-push-3">
+        <div class="tabela-local">
+        <div class="table-responsive"> 
+              <table class="table table-inverse table-hover">
         <thead>
-    <tr>
-      <th>#</th>
+      <tr>
       <th>Dados</th>
-
-    </tr>
+      <th></th>
+      </tr>
         </thead>
 
-                  <tbody>
-                    <tr>
-                      <th scope="row">Latitude</th>
-                     <td> <?= $user_data['latitude']?></td>
-
-                    </tr>
+        <tbody>
+          <tr>
+          <th scope="row">Latitude</th>
+          <td> <?= $user_data['latitude']?></td>
+          </tr>
                    
-                    <tr>
-                      <th scope="row">Longitude</th>
-                      <td><?= $user_data['longitude']?> </td>
-
-                    </tr>
+          <tr>
+          <th scope="row">Longitude</th>
+          <td><?= $user_data['longitude']?> </td>
+          </tr>
                     
-                    <tr>
-                      <th scope="row">Ping</th>
-                      <td> <?= $user_data['ping']?></td>
+          <tr>
+          <th scope="row">Ping</th>
+          <td> <?= $user_data['ping']?></td>
+          </tr>
 
-                    </tr>
+          <tr>
+          <th scope="row">Perda de pacote</th>
+          <td> <?= $user_data['packetloss']?> </td>
+          </tr>
 
-                    <tr>
-                      <th scope="row">Perda de pacote</th>
-                      <td> <?= $user_data['packetloss']?> </td>
+           <tr>
+           <th scope="row">Velocidade download</th>
+           <td> <?= $user_data['download_speed']?></td>
+           </tr>
 
-                    </tr>
+           <tr>
+           <th scope="row">Velocidade upload</th>
+           <td></td>
+           </tr>
 
-                    <tr>
-                      <th scope="row">Velocidade download</th>
-                      <td> <?= $user_data['download_speed']?></td>
+           <tr>
+           <th scope="row">Jitter</th>
+           <td> <?= $user_data['jitter']?></td>
+           </tr>
 
-                    </tr>
-
-                    <tr>
-                      <th scope="row">Velocidade upload</th>
-                      <td></td>
-
-                      </tr>
-
-                    <tr>
-                      <th scope="row">Jitter</th>
-                      <td> <?= $user_data['jitter']?></td>
-                      <td></td>
-
-                    </tr>
-                  </tbody>
-                </table>
-                </div>
-
-      <h1>Análise das suas proximidades</h1>
-      <h4>Aqui estão os dados analisados das 10 redes mais próximas de você</h4> 
-      <hr>
-      <div class="row">
-
-      <table class="table">
-  <thead>
-    <tr>
-      <th>#</th>
-      <th>Latitude</th>
-      <th>Longitude</th>
-      <th>Ping</th>
-      <th>Perda de pacote</th>
-      <th>Velocidade de Download</th>
-      <th>Velocidade de Upload</th>
-    </tr>
-  </thead>
-  <tbody>
-    <tr>
-      <th scope="row">1</th>
-      <td><?php validate_data($nearby_data[0]['latitude']);?>  </td>
-      <td><?php validate_data($nearby_data[0]['longitude']);?></td>
-      <td><?php validate_data($nearby_data[0]['ping']);?> </td>
-      <td><?php validate_data($nearby_data[0]['packetloss']);?>  </td>
-      <td><?php validate_data($nearby_data[0]['download_speed']);?>  </td>
-      <td> </td>
-    </tr>
-    <tr>
-      <th scope="row">2</th>
-      <td><?php validate_data($nearby_data[1]['latitude']);?>  </td>
-      <td><?php validate_data($nearby_data[1]['longitude']);?></td>
-      <td><?php validate_data($nearby_data[1]['ping']);?> </td>
-      <td><?php validate_data($nearby_data[1]['packetloss']);?>  </td>
-      <td><?php validate_data($nearby_data[1]['download_speed']);?>  </td>
-      <td> </td>
-    </tr>
-    <tr>
-      <th scope="row">3</th>
-      <td><?php validate_data($nearby_data[2]['latitude']);?>  </td>
-      <td><?php validate_data($nearby_data[2]['longitude']);?></td>
-      <td><?php validate_data($nearby_data[2]['ping']);?> </td>
-      <td><?php validate_data($nearby_data[2]['packetloss']);?>  </td>
-      <td><?php validate_data($nearby_data[2]['download_speed']);?>  </td>
-      <td> </td>
-    </tr>
-    <tr>
-          <th scope="row">4</th>
-      <td><?php validate_data($nearby_data[3]['latitude']);?>  </td>
-      <td><?php validate_data($nearby_data[3]['longitude']);?></td>
-      <td><?php validate_data($nearby_data[3]['ping']);?> </td>
-      <td><?php validate_data($nearby_data[3]['packetloss']);?>  </td>
-      <td><?php validate_data($nearby_data[3]['download_speed']);?>  </td>
-      <td> </td>
-      </tr>
-       <tr>
-          <th scope="row">5</th>
-      <td><?php validate_data($nearby_data[4]['latitude']);?>  </td>
-      <td><?php validate_data($nearby_data[4]['longitude']);?></td>
-      <td><?php validate_data($nearby_data[4]['ping']);?> </td>
-      <td><?php validate_data($nearby_data[4]['packetloss']);?>  </td>
-      <td><?php validate_data($nearby_data[4]['download_speed']);?>  </td>
-      <td> </td>
-      </tr>
-       <tr>
-          <th scope="row">6</th>
-     <td><?php validate_data($nearby_data[5]['latitude']);?>  </td>
-      <td><?php validate_data($nearby_data[5]['longitude']);?></td>
-      <td><?php validate_data($nearby_data[5]['ping']);?> </td>
-      <td><?php validate_data($nearby_data[5]['packetloss']);?>  </td>
-      <td><?php validate_data($nearby_data[5]['download_speed']);?>  </td>
-      <td> </td>
-      </tr>
-       <tr>
-          <th scope="row">7</th>
-      <td><?php validate_data($nearby_data[6]['latitude']);?>  </td>
-      <td><?php validate_data($nearby_data[6]['longitude']);?></td>
-      <td><?php validate_data($nearby_data[6]['ping']);?> </td>
-      <td><?php validate_data($nearby_data[6]['packetloss']);?>  </td>
-      <td><?php validate_data($nearby_data[6]['download_speed']);?>  </td>
-      <td> </td>
-      </tr>
-       <tr>
-          <th scope="row">8</th>
-      <td><?php validate_data($nearby_data[7]['latitude']);?>  </td>
-      <td><?php validate_data($nearby_data[7]['longitude']);?></td>
-      <td><?php validate_data($nearby_data[7]['ping']);?> </td>
-      <td><?php validate_data($nearby_data[7]['packetloss']);?>  </td>
-      <td><?php validate_data($nearby_data[7]['download_speed']);?>  </td>
-      <td> </td>
-      </tr>
-       <tr>
-          <th scope="row">9</th>
-      <td><?php validate_data($nearby_data[8]['latitude']);?>  </td>
-      <td><?php validate_data($nearby_data[8]['longitude']);?></td>
-      <td><?php validate_data($nearby_data[8]['ping']);?> </td>
-      <td><?php validate_data($nearby_data[8]['packetloss']);?>  </td>
-      <td><?php validate_data($nearby_data[8]['download_speed']);?>  </td>
-      <td> </td>
-      </tr>
-       <tr>
-          <th scope="row">10</th>
-      <td><?php validate_data($nearby_data[9]['latitude']);?>  </td>
-      <td><?php validate_data($nearby_data[9]['longitude']);?></td>
-      <td><?php validate_data($nearby_data[9]['ping']);?> </td>
-      <td><?php validate_data($nearby_data[9]['packetloss']);?>  </td>
-      <td><?php validate_data($nearby_data[9]['download_speed']);?>  </td>
-      <td> </td>
-      </tr>
-  </tbody>
-</table>
-
+            </tbody>
+          </table>
+          </div>
         </div>
-      </div>
-      </div>
-      </div>
-      </section>
+        </div>
 
+        </div> <!--row -->
+   </div> <!--container -->
 
- </div>
-      </div>
+   <aside role="complementary" class="col-md-3 col-md-push-3"></aside>
+   <nav class="col-md-3 col-md-pull-9"></nav>
 
-      </div>
-      </section>
-              
-
+  </section> <!--section tabelas -->
+    
 
     <!-- jQuery (necessary for Bootstrap's JavaScript plugins) -->
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js"></script>
