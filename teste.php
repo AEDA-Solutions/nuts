b21f500f62f9d40bdf67a75b55c8348fa6bda496
SamuelSS09 <samuelss0906@gmail.com>
2017-06-29 21:23:33 -0300

    AAAAAAAAAAA

--- teste.php
+++ teste.php
@@ -1,36 +1,6 @@
-
-<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
-    <!-- Include all compiled plugins (below), or include individual files as needed -->
-    <script src="bootstrap/js/bootstrap.min.js"></script>
-
-      <!-- Scrolling Nav JavaScript -->
-    <script src="bootstrap/js/jquery.easing.min.js"></script>
-    <script src="bootstrap/js/scrolling-nav.js"></script>
-
-    <script>
-
- var arr =  <?php
-  require_once('NetController.php');
-  $NetController = new NetController();
-  $weightArray = $NetController->weightData(); // array com os pesos
-  $data = $NetController->weightCordinates(); // array onde cada elemento é um array com latitude e longitude
-  for($i = 0; $i < sizeof($weightArray); $i++){
-  array_push($data[$i],$weightArray[$i]);
-  } 
-  echo json_encode($data);
-  ?> ; 
-
-  alert("blz");
-
-</script>
-
 <?php
-  require_once('NetController.php');
-  $NetController = new NetController();
-  $weightArray = $NetController->weightData(); // array com os pesos
-  $data = $NetController->weightCordinates(); // array onde cada elemento é um array com latitude e longitude
-  for($i = 0; $i < sizeof($weightArray); $i++){
-    array_push($data[$i],$weightArray[$i]);
-  } 
-  echo json_encode($data);
+echo $_POST['download_speed'];
+echo "<br/>";
+echo $_POST['upload_speed'];
+
 ?>
