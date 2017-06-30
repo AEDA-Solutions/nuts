22407c82d351298db25791398bcc9d94f5dbf999
isabelalobatob <isabela_lobato@hotmail.com>
2017-06-29 21:16:42 -0300

    Rodape

--- index.php
+++ index.php
@@ -282,6 +282,10 @@
      </div>
      </section>
 
+     <footer>
+       NUTS
+     </footer>
+
 
     <!-- jQuery (necessary for Bootstrap's JavaScript plugins) -->
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
